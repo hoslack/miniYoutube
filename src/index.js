7eb4d577505143a4d649893755e9486f30168118
hoslack <hoslackochieng@gmail.com>
2017-12-29 21:58:34 +0200

    video list shown

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import reactDom from 'react-dom';
 import Searchbar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCSqxoBMTdeHnqDEKoGBYng5uGwzPmI8-A';
 
-const App = () => (
-	<div>
-		<Searchbar />
-	</div>
-);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+		YTSearch({ key: API_KEY, term: 'sufboards' }, videos => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<Searchbar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
 
 reactDom.render(<App />, document.querySelector('#container'));
